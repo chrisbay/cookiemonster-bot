2318fef5fdb57f39ad90a7a50edc8646f3731b54
Chris Bay <chris@chrisbay.net>
2019-05-07 16:25:35 -0500

    Refactors for v2 slack api

--- bot.py
+++ bot.py
@@ -5,69 +5,62 @@ import requests
 import json
 import random
 import datetime
-from slackclient import SlackClient
+import slack
 
 
 BOT_ID = os.environ.get("SLACK_BOT_ID")
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-
-
-def handle_command(command, channel, user):
-    if "cake" in command:
-        gif_url = get_random_gif("cookie cake")
-        post_to_slack(channel, "CAKE NOT AS GOOD AS COOKIE. MAYBE IT NUMBER 3 ALL-TIME DESSERT BEHIND COOKIE AND COOKIE CAKE\n", gif_url)
-    elif is_mention(BOT_ID, command):
-        post_to_slack(channel, "WHO YOU CALLING MONSTER?!")  
-    elif "cookie" in command:
-        gif_url = get_random_gif("cookie monster")
-        post_to_slack(channel, "DID SOMEONE SAY *COOKIE*?!\n", gif_url)
 
 
 def get_random_gif(tag):
     """ 
     Fetch a random GIF from Giphy using tag
     """
-    api_key = 'dc6zaTOxFJmzC'
-    url = 'http://api.giphy.com/v1/gifs/random'
-    params = { 'api_key': api_key, 'tag': tag}
-    resp = requests.get(url, params=params)
-    data = resp.json()
-    return  data['data']['url']
+    # api_key = 'dc6zaTOxFJmzC'
+    # url = 'http://api.giphy.com/v1/gifs/random'
+    # params = {'api_key': api_key, 'tag': tag}
+    # resp = requests.get(url, params=params)
+    # data = resp.json()
+    # print(data)
+    # return data['data']['url']
+    return ''
     
 
 def is_mention(id, command):
     """
     Return True if the user with ID id is mentioned in command
     """
-    return id.lower() in command 
+    return id.lower() in command
 
 
-def post_to_slack(channel, msg, url=''):
-    """
-    Post msg to channel, optionally with a url
-    """
-    slack_client.api_call("chat.postMessage", channel=channel, text=(msg + url), as_user=True)
+@slack.RTMClient.run_on(event='message')
+def handle_message(**payload):
+    data = payload['data']
+    command = data['text']
+    channel_id = data['channel']
+    thread_ts = data['ts']
+    user = data['user']
+    msg = ''
 
+    webclient = payload['web_client']
 
-def parse_slack_output(slack_rtm_output):
-    print(slack_rtm_output)
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and output['user'] != BOT_ID:
-                # return text
-                return output['text'].lower(), output['channel'], output['user']
-    return None, None, None
-
+    if "cake" in command:
+        gif_url = get_random_gif("cookie cake")
+        msg = "CAKE NOT AS GOOD AS COOKIE. MAYBE IT NUMBER 3 ALL-TIME DESSERT BEHIND COOKIE AND COOKIE CAKE\n" + gif_url
+    elif is_mention(BOT_ID, command):
+        msg = "WHO YOU CALLING MONSTER?!"
+    elif "cookie" in command:
+        gif_url = get_random_gif("cookie monster")
+        msg = "DID SOMEONE SAY *COOKIE*?!\n" + gif_url
+    
+    if msg:
+        webclient.chat_postMessage(
+            channel=channel_id,
+            text=msg,
+            thread_ts=thread_ts
+        )
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
-    if slack_client.rtm_connect():
-        print("Bot connected and running!")
-        while True:
-            command, channel, user = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel, user)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+
+    slack_token = os.environ["SLACK_BOT_TOKEN"]
+    rtm_client = slack.RTMClient(token=slack_token)
+    rtm_client.start()
